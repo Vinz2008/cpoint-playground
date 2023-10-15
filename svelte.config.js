98e86407d44c9138a5b773fcca4bd839bdf097ef
Vinz2008 <vincentbidarddelanoe@gmail.com>
2023-10-15 21:20:26 +0200

    work on github pages and Dockerfile

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const dev = process.env.NODE_ENV === "production";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +15,10 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	}
+	},
+    paths: {
+        base: dev ? "/cpoint-playground" : "",
+    }
 };
 
 export default config;
